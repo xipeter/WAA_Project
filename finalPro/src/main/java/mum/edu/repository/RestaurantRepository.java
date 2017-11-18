b836aa66e9d00b8be1e74aec05f8c32b92f62658
Samphors Khlok <khlok.samphors@gmail.com>
2017-11-18 16:22:16 -0600

    update query to search by name
    ok...

--- finalPro/src/main/java/mum/edu/repository/RestaurantRepository.java
+++ finalPro/src/main/java/mum/edu/repository/RestaurantRepository.java
@@ -11,6 +11,6 @@ import mum.edu.domain.Restaurant;
 
 @Repository
 public interface RestaurantRepository extends CrudRepository<Restaurant,Long> {
-	@Query("SELECT r FROM Restaurant r WHERE r.name=:name")
+	@Query("SELECT r FROM Restaurant r WHERE r.name LIKE CONCAT('%',:name,'%')")
 	public List<Restaurant> findByName(@Param("name") String name);
 }
