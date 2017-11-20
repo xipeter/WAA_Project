3287128173d6d698eb456af934c94e6a83367d9c
xipeter <xizongfei@gmail.com>
2017-11-19 19:46:07 -0600

    add the logout function

--- finalPro/src/main/java/mum/edu/controller/LoginController.java
+++ finalPro/src/main/java/mum/edu/controller/LoginController.java
@@ -1,6 +1,12 @@
 package mum.edu.controller;
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +26,15 @@ public class LoginController {
 		model.addAttribute("error", "true");
 		return "login";
  
-	}
- 
+	}	
 	@RequestMapping(value="/logout", method = RequestMethod.GET)
-	public String logout(Model model) {
- 		return "redirect:/index";
- 	}
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null){    
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/login";
+	}
+	
+	
 }
