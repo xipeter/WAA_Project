96fd43212f7ed28abfff7f471d838cba9d890c1e
xipeter <xizongfei@gmail.com>
2017-11-21 09:46:59 -0600

    eamil formatter

---
+++ finalPro/src/main/java/mum/edu/controller/EmailControllerTest.java
@@ -0,0 +1,10 @@
+package mum.edu.controller;
+
+import org.junit.runner.RunWith;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("test-DispatcherServlet-context.xml")
+@WebAppConfiguration 
+public class EmailControllerTest {
+	
+}
--- finalPro/src/main/java/mum/edu/serviceimpl/EmailServiceImpl.java
+++ finalPro/src/main/java/mum/edu/serviceimpl/EmailServiceImpl.java
@@ -3,6 +3,7 @@ package mum.edu.serviceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import mum.edu.domain.Email;
@@ -15,11 +16,13 @@ public class EmailServiceImpl implements EmailService {
 	@Autowired
 	EmailRepsitory emailRepository;
 	@Override
+	@PreAuthorize("hasRole('ROLE_RESTAURANT') or hasRole('ROLE_ADMIN')")
 	public void sendEmail(SimpleMailMessage email) {
 		// TODO Auto-generated method stub
 		sender.send(email);
 	}
 	@Override
+	
 	public Email save(Email email) {
 		// TODO Auto-generated method stub
 		return this.emailRepository.save(email);
